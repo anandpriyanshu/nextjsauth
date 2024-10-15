46a353e7fbf2d8380377c3e14f5908057d80e0a1
Priyanshu Anand <anandpriyanshu12@gmail.com>
2024-10-15 10:09:47 +0530

    fix html content for verify user and reset password

--- src/helper/mailer.ts
+++ src/helper/mailer.ts
@@ -25,19 +25,30 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
             host: "sandbox.smtp.mailtrap.io",
             port: 2525,
             auth: {
-                user: "9c8dca428675f6",
-                pass: "9014c47050af55"
+                user: "9c8dca428675f6",  //will put into env
+                pass: "9014c47050af55"  //will put into env
             }
         });
+
+        // // HTML content for verify email
+        const verifyHtml = `
+        <p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to verify your email
+        or copy and paste the link below in your browser.
+        <br>${process.env.DOMAIN}/verifyemail?token=${hashedToken}</p>
+    `;
+
+        // HTML content for RESET email
+        const resetHtml = `
+        <p>Click <a href="${process.env.DOMAIN}/resetpassword?token=${hashedToken}">here</a> to reset your password
+        or copy and paste the link below in your browser.
+        <br>${process.env.DOMAIN}/resetpassword?token=${hashedToken}</p>
+    `;
 
         const mailOptions = {
             from: 'anand@12.ai',
             to: email,
             subject: emailType === "VERIFY" ? "Verify your email" : "Reset you password",
-            html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "Verify your email" : "Reset you password"} 
-            or copy and paste the link below in your browser.
-            <br>${process.env.DOMAIN}/verifyemail?token=${hashedToken}
-            </p>`,
+            html: emailType === 'VERIFY' ? verifyHtml : resetHtml,
         }
 
         const mailResponse = await transport.sendMail(mailOptions)
